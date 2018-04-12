907281c5cdac8180486ed7af31ae822f60db9ddf
Emily Fortuna <efortuna@google.com>
2018-04-12 16:37:38 -0700

    fixed up coverflow to take callbacks and a widget builder.

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Coverflow Demo',
       theme: new ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: new MyHomePage(),
@@ -18,26 +17,52 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  static List<Container> data = [
+    new Container(color: Colors.orange),
+    new Container(color: Colors.blue),
+    new Container(color: Colors.amber),
+    new Container(color: Colors.deepPurple),
+    new Container(color: Colors.green),
+    new Container(color: Colors.red),
+    new Container(color: Colors.yellow),
+    new Container(color: Colors.greenAccent),
+    new Container(color: Colors.black)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: new Text('coverflow demo'),
+        title: new Text('Coverflow Demo'),
       ),
-      body: new CoverFlow([new Container(color: Colors.deepOrangeAccent),
-      new Container(color: Colors.blue),
-      new Container(color: Colors.amber),
-      new Container(color: Colors.deepPurpleAccent)]), // This trailing comma makes auto-formatting nicer for build methods.
+      body: new CoverFlow(widgetBuilder,
+          disposeDismissed), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget widgetBuilder(BuildContext context, int index) {
+    if (data.length == 0) {
+      return new Container();
+    } else {
+      return data[index % data.length];
+    }
+  }
+
+  disposeDismissed(int index, DismissDirection direction) {
+    data.removeAt(index);
   }
 }
 
+typedef void OnDismissedCallback(
+    int itemDismissedIndex, DismissDirection direction);
 
 class CoverFlow extends StatefulWidget {
-  List displayCovers;
-  CoverFlow(this.displayCovers);
+  IndexedWidgetBuilder itemBuilder;
+  OnDismissedCallback dismissedCallback;
+
+  CoverFlow(this.itemBuilder, this.dismissedCallback);
 
   @override
   _CoverFlowState createState() => new _CoverFlowState();
@@ -45,9 +70,8 @@ class CoverFlow extends StatefulWidget {
 
 class _CoverFlowState extends State<CoverFlow> {
   PageController controller;
-  int currentpage = 0;
+  int currentPage = 0;
   bool _pageHasChanged = false;
-
 
   @override
   initState() {
@@ -67,7 +91,7 @@ class _CoverFlowState extends State<CoverFlow> {
         onPageChanged: (value) {
           setState(() {
             _pageHasChanged = true;
-            currentpage = value;
+            currentPage = value;
           });
         },
         controller: controller,
@@ -76,9 +100,9 @@ class _CoverFlowState extends State<CoverFlow> {
 
   builder(int index) {
     return new AnimatedBuilder(
-      animation: controller,
-      builder: (context, Widget child) {
-        double result = _pageHasChanged? controller.page : 0.0;
+        animation: controller,
+        builder: (context, Widget child) {
+          double result = _pageHasChanged ? controller.page : 0.0;
           double value = result - index;
 
           value = (1 - (value.abs() * .5)).clamp(0.0, 1.0);
@@ -90,23 +114,18 @@ class _CoverFlowState extends State<CoverFlow> {
               child: new SizedBox(
                 height: Curves.easeOut.transform(value) * 525,
                 width: Curves.easeOut.transform(value) * 700,
-                child: new Stack(children: [
-                  child,
-                  new Container(color: Colors.transparent), ]),
-
+                child: child,
               ),
             ),
             onDismissed: (direction) {
               setState(() {
-                controller.nextPage(duration: new Duration(seconds: 1), curve: Curves.easeOut);
-                widget.displayCovers.removeAt(0);
+                widget.dismissedCallback(currentPage, direction);
+                controller.animateToPage(currentPage,
+                    duration: new Duration(seconds: 2), curve: Curves.easeOut);
               });
             },
           );
-
-      },
-      child: widget.displayCovers[index % widget.displayCovers.length],
-    );
+        },
+        child: widget.itemBuilder(context, index));
   }
 }
-
