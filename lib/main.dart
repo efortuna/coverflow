b188d0cca71042f7a3f19ec659866c0f995b3360
Emily Fortuna <efortuna@google.com>
2018-04-12 12:19:32 -0700

    taking out catch statement so the error is obvious

--- lib/main.dart
+++ lib/main.dart
@@ -9,79 +9,31 @@ class MyApp extends StatelessWidget {
     return new MaterialApp(
       title: 'Flutter Demo',
       theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
+
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => new _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return new Scaffold(
       appBar: new AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
+        title: new Text('coverflow demo'),
       ),
       body: new CoverFlow([new Container(color: Colors.deepOrangeAccent),
       new Container(color: Colors.blue),
       new Container(color: Colors.amber),
-      new Container(color: Colors.deepPurpleAccent)]),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      new Container(color: Colors.deepPurpleAccent)]), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
 
 class CoverFlow extends StatefulWidget {
   List displayCovers;
@@ -125,7 +77,7 @@ class _CoverFlowState extends State<CoverFlow> {
       animation: controller,
       builder: (context, Widget child) {
         double result = 0.0;
-        try {
+        //if (controller.hasClients) {
           result = controller.page;
           double value = result - index;
 
@@ -151,10 +103,8 @@ class _CoverFlowState extends State<CoverFlow> {
               });
             },
           );
-        } catch (ArgumentError) {
-          // Trying to build before everything has been initialized
-          return new Container();
-        }
+        //}
+        return new Container();
 
       },
       child: widget.displayCovers[index % widget.displayCovers.length],
